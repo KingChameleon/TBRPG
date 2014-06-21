cab03780ddced62e46fd88a716131f7a6ee8b1c1
KingChameleon <stvnlrsn@gmail.com>
2014-06-21 12:52:12 -0700

    combat function
    Adding some exp changes to enemies

--- TBRPG.py
+++ TBRPG.py
@@ -23,11 +23,54 @@ def combat(new_enemy):
     x = roll.dice(1, 20, 0)
     if x > 10:
         print(new_enemy.lvl)
-        print("You win " + str(new_enemy.xp) + " points!!!")
-        steven.xp = steven.xp + new_enemy.xp
-        level_up(steven.xp)
-        print(stats(steven.lvl, steven.xp))
-        roaming()
+        new_enemy = Enemy(random.randint((steven.lvl), (steven.lvl + 1)), 10)
+        if new_enemy.lvl == 1:
+            print("Enemy level is: " + str(new_enemy.lvl))
+            print("You win " + str(new_enemy.xp) + " points!!!")
+            steven.xp = steven.xp + new_enemy.xp
+            level_up(steven.xp)
+            print(stats(steven.lvl, steven.xp))
+            roaming()
+        elif new_enemy.lvl == 2:
+            new_enemy.xp = 15
+            print("Enemy level is: " + str(new_enemy.lvl))
+            print("You win " + str(new_enemy.xp) + " points!!!")
+            steven.xp = steven.xp + new_enemy.xp
+            level_up(steven.xp)
+            print(stats(steven.lvl, steven.xp))
+            roaming()
+        elif new_enemy.lvl == 3:
+            new_enemy.xp = 20
+            print("Enemy level is: " + str(new_enemy.lvl))
+            print("You win " + str(new_enemy.xp) + " points!!!")
+            steven.xp = steven.xp + new_enemy.xp
+            level_up(steven.xp)
+            print(stats(steven.lvl, steven.xp))
+            roaming()
+        elif new_enemy.lvl == 4:
+            new_enemy.xp = 25
+            print("Enemy level is: " + str(new_enemy.lvl))
+            print("You win " + str(new_enemy.xp) + " points!!!")
+            steven.xp = steven.xp + new_enemy.xp
+            level_up(steven.xp)
+            print(stats(steven.lvl, steven.xp))
+            roaming()
+        elif new_enemy.lvl == 5:
+            new_enemy.xp = 30
+            print("Enemy level is: " + str(new_enemy.lvl))
+            print("You win " + str(new_enemy.xp) + " points!!!")
+            steven.xp = steven.xp + new_enemy.xp
+            level_up(steven.xp)
+            print(stats(steven.lvl, steven.xp))
+            roaming()
+        elif new_enemy.lvl == 6:
+            new_enemy.xp = 40
+            print("Enemy level is: " + str(new_enemy.lvl))
+            print("You win " + str(new_enemy.xp) + " points!!!")
+            steven.xp = steven.xp + new_enemy.xp
+            level_up(steven.xp)
+            print(stats(steven.lvl, steven.xp))
+            roaming()
     else:
         print("You lost the battle... :(")
         roaming()
